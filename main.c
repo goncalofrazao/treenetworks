e8707597209c87cb8c853f7fc85967a1d857b2fb
Gonçalo Frazão <gonfrazao@gmail.com>
2023-03-03 20:47:07 +0000

    initialize of fd_set struct and calling argument checker

--- main.c
+++ main.c
@@ -4,12 +4,28 @@
 
 #include "requests.h"
 #include "commands.h"
+#include "command_line.h"
 
 int main(int argc, char *argv[])
 {
     char buffer[MAX_LINE];
     CAAS command;
-    while (fgets(buffer, MAX_LINE, stdin))
+
+    fd_set current_fds, ready_fds;
+    
+    // validate command-line arguments
+    if (!valid_command_line(argc, argv))
+    {
+        print("USAGE: ./cot IP TCP regIP regUDP\n");
+    }
+
+    // initialize current set
+    FD_ZERO(&current_fds);
+    FD_SET(0, &current_fds);
+
+    // deal with interruptions
+    
+    while(fgets(buffer, MAX_LINE, stdin))
     {
         get_command(buffer, &command);
         switch (command.command)
@@ -30,5 +46,7 @@ int main(int argc, char *argv[])
             break;
         }
     }
+    
+    // close fds
     
 }
